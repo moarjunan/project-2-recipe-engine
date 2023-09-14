d1bfbc4154bbf9d12bf458229235c670178df0ba
v4nessa04 <v4nessa04@gmail.com>
2023-09-13 22:20:06 -0400

    added new files

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ const searchResultDiv = document.querySelector('.search-result');
 const container = document.querySelector('container');
 let searchQuery = '';
 const APP_ID ='a3585e31';
-const APP_KEY ='5fe536ef5571a50ad8e9e985775f6a17'
+const APP_KEY ='5fe536ef5571a50ad8e9e985775f6a17';
 
 
 searchForm.addEventListener('submit',(e)=>{
@@ -12,6 +12,18 @@ searchForm.addEventListener('submit',(e)=>{
     fetchAPI();
 });
 
-async function fetchAPI(){
-    const baseURL ='https://api.edamam.com/api/recipes/v2?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}';
+async function fetchAPI (){
+    const baseURL ='https://api.edamam.com/api/recipes/v2?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&to=20';
+    const response = await fetch(baseURL);
+    const data = await response.json();
+    generateHTML(data.hits);
+    console.log(data);
+}
+function generateHTML(results){
+    let generatedHTML = '';
+    results.map(result => {
+        
+ 
+    })
+    searchResultDiv.innerHTML = generateHTML;
 }
